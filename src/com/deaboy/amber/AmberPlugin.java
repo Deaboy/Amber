cd7ae6cf7c06e15ee8c669fd1fade5519a5fb671
Deaboy <danieleandrus@gmail.com>
2012-08-19 00:51:18 -0600

    Initial Commit

---
+++ src/com/deaboy/amber/AmberPlugin.java
@@ -0,0 +1,20 @@
+package com.deaboy.amber;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AmberPlugin extends JavaPlugin
+{
+
+	@Override
+	public void onEnable()
+	{
+		
+	}
+
+	@Override
+	public void onDisable()
+	{
+		
+	}
+
+}
